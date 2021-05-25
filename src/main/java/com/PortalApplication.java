0f4881e1f3d9ceb4133402e9b58a1c71e7d1bfad
Ahmed-zaki1 <zaki40886@gmail.com>
2021-05-25 21:11:16 +0530

    Initial Commit

---
+++ src/main/java/com/PortalApplication.java
@@ -0,0 +1,20 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.entiry.*"})
+@EntityScan(basePackages = {"com.entity.*"})
+@EnableJpaRepositories(basePackages = {"com.repository.*"})
+public class PortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PasswordGenerator.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordGenerator {
+public static void main(String[] args) {
+	BCryptPasswordEncoder encoder=new BCryptPasswordEncoder();
+	String raw="";
+	String encodedpassword = encoder.encode(raw);
+	System.out.println(encodedpassword);
+}
+}
